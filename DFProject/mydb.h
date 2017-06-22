830ba8532f1c2adbe7bf1e25d9a527ffac19d7c8
Ron <huang.rong.bj@gmail.com>
2017-06-22 15:45:50 -0400

    ghh
    ggg

--- DFProject/mydb.h
+++ DFProject/mydb.h
@@ -2,10 +2,9 @@
 #define MYDB_H
 
 #include <QObject>
-<<<<<<< HEAD
+
 //#include "webrequest.h"
-=======
->>>>>>> 4cea3968911170e48f6dd686bfaf35ca1046a38c
+
 
 class MyDB : public QObject
 {
@@ -13,7 +12,7 @@ class MyDB : public QObject
 public:
 	explicit MyDB(QObject *parent = 0);
 
-<<<<<<< HEAD
+
 
 	Q_INVOKABLE QString getName() { return "MyDB Class";}
 	Q_INVOKABLE int getResult(int seed) { return seed *2;}
@@ -22,15 +21,12 @@ signals:
 public slots:
 
 private:
-	//WebRequest req;
-};
 
-#endif // MYDB_H
-=======
+
 signals:
 
 public slots:
 };
 
 #endif // MYDB_H
->>>>>>> 4cea3968911170e48f6dd686bfaf35ca1046a38c
+
